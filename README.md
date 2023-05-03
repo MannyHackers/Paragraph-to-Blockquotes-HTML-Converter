400fe0c925f016fa2ed049335d612413e92ad777
MannyHackers <mannykumar47018@gmail.com>
2023-05-03 08:31:21 +0530

    Paragraph to Blockquotes HTML Converter

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# Paragraph-to-Blockquotes-HTML-Converter
+# Paragraph to Blockquotes HTML Converter
+
 This is a simple web-based tool that allows you to convert a paragraph to blockquotes HTML code. You can use this tool to quickly convert a paragraph to HTML code that you can use on your website or blog.
+
+## Features
+
+- Convert a paragraph to blockquotes HTML code
+- Preview the HTML output in a new window/tab
+- Download the HTML output as a file
+- Copy the HTML output to the clipboard
+- Clear the HTML output from the output div
+- Toggle dark mode for better readability
+
+## Installation
+
+You can install this tool by cloning the repository:
+
+git clone https://github.com/MannyHackers/paragraph-to-blockquotes-html-converter.git
+
+## Usage
+
+1. Open `index.html` in your web browser.
+2. Copy the paragraph that you want to convert to blockquotes HTML code.
+3. Paste the paragraph into the input text area.
+4. Click the "Convert to Blockquotes" button to convert the paragraph to blockquotes HTML code.
+5. Use the other buttons to preview, download, copy, or clear the HTML output as needed.
+
+## Validation
+
+This script has validation for each button to ensure that the user enters a paragraph before performing any action. If the input is empty, it will show an error message and prevent the action from being executed.
+
+## Screenshots
+
+![Alt Text](./Screenshots/ss.png)
+*Screenshot of the tool.*
+
+![Alt Text](./Screenshots/dark_ss.png)
+*Dark Screenshot of the tool.*
+
+## Contributing
+
+Contributions are welcome! If you would like to contribute to this project, please fork the repository and submit a pull request.
+
+## Issues
+
+If you find any issues with this script, please report them [here](https://github.com/MannyHackers/paragraph-to-blockquotes-html-converter/issues).
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+## Credits
+
+This script was created by [MannyHackers](https://github.com/MannyHackers)
+
+## Support
+
+If you need any assistance with this script, please contact [mstorrentdrive@gmail.com](mailto:mstorrentdrive@gmail).
---
+++ script.js
@@ -0,0 +1,155 @@
+// Get the HTML elements from the DOM
+const paragraph = document.getElementById('paragraph');
+const output = document.getElementById('output');
+const convertBtn = document.getElementById('convert-btn');
+const previewBtn = document.getElementById('preview-btn');
+const downloadBtn = document.getElementById('download-btn');
+const copyBtn = document.getElementById('copy-btn');
+const clearBtn = document.getElementById('clear-btn');
+const darkModeBtn = document.getElementById('dark-mode-btn');
+
+// Convert Paragraph to Blockquotes HTML Function
+
+function convertToBlockQuotes() {
+    // Get the input paragraph from the text area
+    const inputParagraph = paragraph.value;
+
+    if (inputParagraph === '') {
+        // Show an error message if the input paragraph is empty
+        alert('Please enter a paragraph before converting.');
+        return;
+    }
+
+    // Split the paragraph into separate lines
+    const lines = inputParagraph.split(/\r?\n/);
+
+    // Create an empty string to hold the HTML output
+    let html = '';
+
+    // Loop through each line and wrap it in a blockquote element
+    lines.forEach(line => {
+        html += `<blockquote class="wp-block-quote">\n`;
+        html += `  <p class="has-text-align-center">${line}</p>\n`;
+        html += `</blockquote>\n`;
+    });
+
+    // Set the HTML output to the output div
+    output.innerHTML = html;
+
+    // Show a success message
+    alert('Converted Successfully!');
+}
+
+// Preview HTML Codes Function
+function previewHtmlCodes() {
+    // Get the HTML output from the output div
+    const html = output.innerHTML;
+
+    if (html === '') {
+        // Show an error message if the HTML output is empty
+        alert('Please convert a paragraph to HTML before previewing.');
+        return;
+    }
+
+    // Open a new window/tab and display the HTML output
+    const previewWindow = window.open();
+    previewWindow.document.write(`
+        <html>
+            <head>
+                <title>HTML Output Preview</title>
+            </head>
+            <body>
+                <h2>HTML Output Preview</h2>
+                <pre>${output.innerHTML}</pre>
+            </body>
+        </html>
+    `);
+}
+
+// Download HTML Function
+function downloadHtml() {
+    // Get the HTML output from the output div
+    const html = output.innerHTML;
+	
+    if (html === '') {
+    	// Show an error message if the HTML output is empty
+        alert('Please convert a paragraph to HTML before downloading.');
+        return;
+    }
+    // Create a new blob with the HTML content and set the MIME type to text/plain
+    const blob = new Blob([html], { type: 'text/plain' });
+
+    // Create a new URL with the blob data
+    const url = URL.createObjectURL(blob);
+
+    // Create a new link element and set its properties
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'BlockQuotes.html';
+
+    // Append the link element to the document body and trigger a click event
+    document.body.appendChild(link);
+    link.click();
+
+    // Remove the link element from the document body and revoke the URL
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
+
+    // Show a success message
+    alert('Downloaded Successfully!');
+}
+
+// Copy HTML Function
+function copyHtml() {
+    // Get the HTML output from the output div
+    const html = output.innerHTML;
+	
+    if (html === '') {
+        // Show an error message if the HTML output is empty
+        alert('Please convert a paragraph to HTML before copying.');
+        return;
+    }
+	
+    // Create a new textarea element and set its properties
+    const textarea = document.createElement('textarea');
+    textarea.value = html;
+    textarea.setAttribute('readonly', '');
+    textarea.style.position = 'absolute';
+    textarea.style.left = '-9999px';
+
+    // Append the textarea element to the document body and select its contents
+    document.body.appendChild(textarea);
+    textarea.select();
+
+    // Copy the selected text to the clipboard
+    document.execCommand('copy');
+    
+    // Remove the textarea element from the document body
+    document.body.removeChild(textarea);
+
+    // Show a success message
+    alert('HTML Output Copied to Clipboard!');
+}
+
+// Clear Function
+function clearOutput() {
+    // Clear the input paragraph and the HTML output
+    paragraph.value = '';
+    output.innerHTML = '';
+	// Show a success message
+    alert('Output Cleared!');
+}
+
+// Toggle Dark Mode Function
+function toggleDarkMode() {
+  const body = document.body;
+  body.classList.toggle('dark-mode');
+}
+// Add Event Listeners to the Buttons
+convertBtn.addEventListener('click', convertToBlockQuotes);
+previewBtn.addEventListener('click', previewHtmlCodes);
+downloadBtn.addEventListener('click', downloadHtml);
+copyBtn.addEventListener('click', copyHtml);
+clearBtn.addEventListener('click', clearOutput);
+darkModeBtn.addEventListener('click', toggleDarkMode);
+
